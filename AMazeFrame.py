72e57c79914dd833944526b32de5a082fca1e301
Samuel Loubiat <sloubiat@z4r8p1.42lyon.fr>
2026-03-27 17:43:51 +0100

    add entry and exit

--- AMazeFrame.py
+++ AMazeFrame.py
@@ -10,6 +10,8 @@ class AMazeFrame:
         self.__mx = Mlx()
         self.__mlx_ptr = self.__mx.mlx_init()
         self.__img = self.__mx.mlx_new_image(self.__mlx_ptr, width, height)
+        self.current_idx = 0
+        self.offset = 25
 
     def close(self, keytype, param):
         if keytype == 113:
@@ -64,7 +66,16 @@ class AMazeFrame:
                     if 0 <= target_x < self.__width:
                         addr[i * stride + target_x] = color
 
-    def __generate_maze(self, maze: str):
+    def draw_cube(self, addr, stride, x_from, x_to, y_from, y_to, color):
+        for x in range(x_from, x_to):
+            for y in range(y_from, y_to):
+                addr[y * stride + x] = color
+
+
+    def __generate_maze(self, maze_dict: dict):
+        maze = maze_dict['maze']
+        maze_entry = maze_dict['maze_entry']
+        maze_exit = maze_dict['maze_exit']
         img_w = self.__width - 50
         img_h = self.__height - 50
         addr_raw, bpp, line_size, endian = self.__mx.mlx_get_data_addr(self.__img)
@@ -93,10 +104,16 @@ class AMazeFrame:
         for char in maze:
             if char == '\n':
                 x = 0
+                self.__mx.mlx_put_image_to_window(self.__mlx_ptr,
+                                                  self.__mlx_window,
+                                                  self.__img, 0, 0)
                 y += 1
                 continue
 
             bits = format(int(char, 16), '04b')
+            if (x, y) == maze_entry or (x, y) == maze_exit:
+                self.draw_cube(addr, stride, x * cell_x + offset, x * cell_x + offset + cell_x,
+                               cell_y * y + offset, cell_y * y + offset + cell_y,0x123456FF)
             if bits[0] == '1':  # Mur Nord
                 self.draw_x(addr, stride, x * cell_x + offset, x * cell_x + offset + cell_x,
                             cell_y * y + offset, 0xFFFFFFFF)
@@ -111,13 +128,12 @@ class AMazeFrame:
                             x * cell_x + offset, 0xFFFFFFFF)
 
             x += 1
-            self.__mx.mlx_put_image_to_window(self.__mlx_ptr, self.__mlx_window,
-                                              self.__img, 0, 0)
+
 
         #self.__mx.mlx_put_image_to_window(self.__mlx_ptr, self.__mlx_window, self.__img, 0, 0)
 
-    def generate_maze(self, maze: str):
-        self.__mx.mlx_loop_hook(self.__mlx_ptr, self.__generate_maze, maze)
+    def generate_maze(self, maze: str, maze_entry: tuple[int, int], maze_exit: tuple[int, int]):
+        self.__mx.mlx_loop_hook(self.__mlx_ptr, self.__generate_maze, {'maze': maze, 'maze_entry': maze_entry, 'maze_exit': maze_exit})
 
     def start_loop(self):
         self.__mx.mlx_loop(self.__mlx_ptr)
