cf53154442279582b10ab84b3ed673842f32a50f
Samuel Loubiat <sloubiat@z4r9p5.42lyon.fr>
2026-03-31 17:46:38 +0200

    wip

--- AMazeFrame.py
+++ AMazeFrame.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from mlx import Mlx
 
 
@@ -14,15 +16,15 @@ class AMazeFrame:
         self.offset = 25
         self.__paff = False
 
-    def close(self, keytype, param):
-        if keytype == 113:
+    def close(self, *param: Any) -> None:
+        if param[0] == 113:
             self.__mx.mlx_destroy_image(self.__mlx_ptr, self.__img)
             self.__mx.mlx_destroy_window(self.__mlx_ptr, self.__mlx_window)
             self.__mx.mlx_loop_exit(self.__mlx_ptr)
-        if keytype == 112:
+        if param[0] == 112:
             self.__paff = not self.__paff
 
-    def close_icon(self, param):
+    def close_icon(self, _: Any) -> None:
         self.__mx.mlx_destroy_image(self.__mlx_ptr, self.__img)
         self.__mx.mlx_destroy_window(self.__mlx_ptr, self.__mlx_window)
         self.__mx.mlx_loop_exit(self.__mlx_ptr)
@@ -32,54 +34,82 @@ class AMazeFrame:
                                                      self.__width,
                                                      self.__height, title)
         self.__mx.mlx_hook(self.__mlx_window, 2, 1, self.close, self)
-        self.__mx.mlx_hook(self.__mlx_window, 33, 0, self.close_icon, None)
+        self.__mx.mlx_hook(self.__mlx_window, 33, 0, self.close_icon, self)
 
-    def add_pixel(self, addr, x, y, stride, color):
-        addr[y * stride + x] = color
+    def add_pixel(self, x: int, y: int, color: int) -> None:
+        self.__addr[y * self.__stride + x] = color
 
-    def __generate_background(self, color):
-        addr_raw, bpp, line_size, endian = self.__mx.mlx_get_data_addr(
-            self.__img)
-
-        addr = addr_raw.cast('I')
-        stride = line_size // (bpp // 8)
+    def __generate_background(self, color: Any) -> None:
         for y in range(self.__height):
             for x in range(self.__width):
-                self.add_pixel(addr, x, y, stride, color)
+                self.add_pixel(x, y, color)
 
-    def generate_background(self, color) -> None:
+    def generate_background(self, color: Any) -> None:
         self.__mx.mlx_loop_hook(self.__mlx_ptr, self.__generate_background,
                                 color)
 
-    def draw_x(self, addr, stride, x_start, x_end, y, color):
+    def draw_x(self, x_start: int, x_end: int, y: int, color: Any) -> None:
         for x in range(x_start, x_end):
-            for i in range(4):
-                addr[(y + self.offset + i) * stride
-                     + (x + self.offset)] = color
+            for i in range(1):
+                self.__addr[(y + self.offset + i) * self.__stride
+                            + (x + self.offset)] = color
 
-    def draw_y(self, addr, stride, y_start, y_end, x, color):
+    def draw_y(self, y_start: int, y_end: int, x: int, color: Any) -> None:
         for y in range(y_start, y_end):
-            for i in range(4):
-                addr[(y + self.offset) * stride
-                     + (x + self.offset + i)] = color
+            for i in range(1):
+                self.__addr[(y + self.offset) * self.__stride
+                            + (x + self.offset + i)] = color
 
-    def draw_cube(self, addr, stride, x_from, x_to, y_from, y_to, color):
+    def draw_cube(self, x_from: int, x_to: int, y_from: int, y_to: int,
+                  color: Any) -> None:
         for x in range(x_from, x_to):
             for y in range(y_from, y_to):
-                addr[(y + self.offset) * stride + (x + self.offset)] = color
+                self.__addr[(y + self.offset) * self.__stride + (
+                        x + self.offset)] = color
 
-    def __generate_maze(self, maze_dict: dict):
+    def draw_connection(self, last_char: str, cell: tuple[int, int],
+                        color: Any) -> None:
+        x, y = cell
+        if last_char == 'N':
+            self.draw_cube(x * self.cell_x + (self.cell_x // 4),
+                           (x + 1) * self.cell_x - (self.cell_x // 4),
+                           y * self.cell_y + (self.cell_y // 2),
+                           (y + 2) * self.cell_y - (self.cell_y // 2),
+                           color)
+        if last_char == 'S':
+            self.draw_cube(x * self.cell_x + (self.cell_x // 4),
+                           (x + 1) * self.cell_x - (self.cell_x // 4),
+                           (y - 1) * self.cell_y + (self.cell_y // 2),
+                           (y + 1) * self.cell_y - (self.cell_y // 2),
+                           color)
+        if last_char == 'W':
+            self.draw_cube(x * self.cell_x + (self.cell_x // 2),
+                           (x + 2) * self.cell_x - (self.cell_x // 2),
+                           y * self.cell_y + (self.cell_y // 4),
+                           (y + 1) * self.cell_y - (self.cell_y // 4),
+                           color)
+        if last_char == 'E':
+            self.draw_cube((x - 1) * self.cell_x + (self.cell_x // 2),
+                           (x + 1) * self.cell_x - (self.cell_x // 2),
+                           y * self.cell_y + (self.cell_y // 4),
+                           (y + 1) * self.cell_y - (self.cell_y // 4),
+                           color)
+
+    def __generate_maze(self, maze_dict: dict[Any, Any]) -> None:
         maze = maze_dict['maze']
         maze_entry = maze_dict['maze_entry']
         maze_exit = maze_dict['maze_exit']
         resolver = maze_dict['maze_resolv']
+        wall_color = maze_dict['wall_color']
+        paff_color = maze_dict['paff_color']
+        background_color = maze_dict['background_color']
         img_w = self.__width - 50
         img_h = self.__height - 50
         addr_raw, bpp, line_size, endian = self.__mx.mlx_get_data_addr(
             self.__img)
 
-        addr = addr_raw.cast('I')
-        stride = line_size // (bpp // 8)
+        self.__addr = addr_raw.cast('I')
+        self.__stride = line_size // (bpp // 8)
 
         maze_size_x = 0
         maze_size_y = 1
@@ -93,123 +123,74 @@ class AMazeFrame:
             if char == '\n':
                 maze_size_y += 1
 
-        cell_x = img_w // maze_size_x
-        cell_y = img_h // maze_size_y
+        self.cell_x = img_w // maze_size_x
+        self.cell_y = img_h // maze_size_y
         x = 0
         y = 0
         for char in maze:
             if char == '\n':
                 x = 0
-                # self.__mx.mlx_put_image_to_window(self.__mlx_ptr,
-                #                                  self.__mlx_window,
-                #                                  self.__img, 0, 0)
                 y += 1
                 continue
 
-            self.draw_cube(addr, stride, x * cell_x, x * cell_x + cell_x,
-                           cell_y * y, cell_y * y + cell_y, 0x1F000000)
+            self.draw_cube(x * self.cell_x,
+                           x * self.cell_x + self.cell_x,
+                           self.cell_y * y, self.cell_y * y + self.cell_y,
+                           background_color)
             bits = int(char, 16)
             if (x, y) == maze_entry or (x, y) == maze_exit:
-                self.draw_cube(addr, stride, x * cell_x, x * cell_x + cell_x,
-                               cell_y * y, cell_y * y + cell_y, 0x123456FF)
+                self.draw_cube(x * self.cell_x, x * self.cell_x + self.cell_x,
+                               self.cell_y * y, self.cell_y * y + self.cell_y,
+                               0x5F3456FF)
             if bits == 15:
-                self.draw_cube(addr, stride, x * cell_x, x * cell_x + cell_x,
-                               cell_y * y, cell_y * y + cell_y, 0xFFFFFFFF)
+                self.draw_cube(x * self.cell_x, x * self.cell_x + self.cell_x,
+                               self.cell_y * y, self.cell_y * y + self.cell_y,
+                               wall_color)
                 x += 1
                 continue
             if bits & (1 << 0):  # Mur Nord
-                self.draw_x(addr, stride, x * cell_x, x * cell_x + cell_x,
-                            cell_y * y, 0xFFFFFFFF)
+                self.draw_x(x * self.cell_x, (x * self.cell_x + self.cell_x),
+                            self.cell_y * y, wall_color)
             if bits & (1 << 2):  # Mur Sud
-                self.draw_x(addr, stride, x * cell_x, x * cell_x + cell_x + 4,
-                            cell_y * y + cell_y, 0xFFFFFFFF)
+                self.draw_x(x * self.cell_x, x * self.cell_x + self.cell_x,
+                            self.cell_y * y + self.cell_y, wall_color)
             if bits & (1 << 1):  # Mur Est
-                self.draw_y(addr, stride, cell_y * y, cell_y * y + cell_y,
-                            x * cell_x + cell_x, 0xFFFFFFFF)
+                self.draw_y(self.cell_y * y, self.cell_y * y + self.cell_y,
+                            x * self.cell_x + self.cell_x, wall_color)
             if bits & (1 << 3):  # Mur Ouest
-                self.draw_y(addr, stride, cell_y * y, cell_y * y + cell_y,
-                            x * cell_x, 0xFFFFFFFF)
-            #self.__mx.mlx_put_image_to_window(self.__mlx_ptr,
-            #                                  self.__mlx_window,
-            #                                  self.__img, 0, 0)
-
+                self.draw_y(self.cell_y * y, self.cell_y * y + self.cell_y,
+                            x * self.cell_x, wall_color)
             x += 1
 
         x, y = maze_entry
-        x_end, y_end = maze_exit
-        last_char: str | None = None
+        last_char: str = ''
         if self.__paff:
             for char in resolver:
+                if last_char is not '':
+                    self.draw_connection(last_char, (x, y), paff_color)
                 if char == 'N':
-                    if last_char is None:
-                        pass
-                    else:
-                        for i in range(-4, 3):
-                            self.draw_y(addr, stride, cell_y * y,
-                                        cell_y * y + (cell_y // 2),
-                                        x * cell_x + (cell_x // 2) + i,
-                                        0xFFFF0000)
                     y -= 1
                 elif char == 'S':
-                    if last_char is None:
-                        for i in range(-4, 3):
-                            self.draw_y(addr, stride,
-                                        cell_y * (y + 1) - (cell_y // 2),
-                                        cell_y * (y + 1),
-                                        x * cell_x + (cell_x // 2) + i,
-                                        0xFFFF0000)
-                        self.draw_cube(addr, stride, x * cell_x
-                                       + (cell_x // 5),
-                                       x * cell_x + cell_x - (cell_x // 5),
-                                       cell_y * y + (cell_y // 5),
-                                       cell_y * y + cell_y - (cell_y // 5),
-                                       0xFFFF0000)
-                    else:
-                        for i in range(-4, 3):
-                            self.draw_y(addr, stride,
-                                        cell_y * (y + 1) - (cell_y // 2),
-                                        cell_y * (y + 1),
-                                        x * cell_x + (cell_x // 2) + i,
-                                        0xFFFF0000)
                     y += 1
                 elif char == 'W':
-                    if last_char is None:
-                        pass
-                    else:
-                        for i in range(-4, 3):
-                            self.draw_x(addr, stride, cell_x * x,
-                                        cell_x * x + (cell_x // 2),
-                                        y * cell_y + (cell_y // 2) + i,
-                                        0xFFFF0000)
                     x -= 1
                 elif char == 'E':
-                    if last_char is None:
-                        pass
-                    else:
-                        for i in range(-4, 3):
-                            self.draw_x(addr, stride,
-                                        cell_x * x + (cell_x // 2),
-                                        cell_x * (x + 1),
-                                        y * cell_y + (cell_y // 2) + i,
-                                        0xFFFF0000)
                     x += 1
                 last_char = char
-                if x == x_end and y == y_end:
-                    self.draw_cube(addr, stride, x * cell_x + (cell_x // 5),
-                                   x * cell_x + cell_x - (cell_x // 5),
-                                   cell_y * y + (cell_y // 5),
-                                   cell_y * y + cell_y - (cell_y // 5),
-                                   0xFFFF0000)
+            self.draw_connection(last_char, (x, y), paff_color)
         self.__mx.mlx_put_image_to_window(self.__mlx_ptr,
                                           self.__mlx_window,
                                           self.__img, 0, 0)
 
     def generate_maze(self, maze: str, maze_entry: tuple[int, int],
-                      maze_exit: tuple[int, int], maze_resolv: str):
+                      maze_exit: tuple[int, int], maze_resolv: str) -> None:
         self.__mx.mlx_loop_hook(self.__mlx_ptr, self.__generate_maze,
                                 {'maze': maze, 'maze_entry': maze_entry,
                                  'maze_exit': maze_exit,
-                                 'maze_resolv': maze_resolv})
+                                 'maze_resolv': maze_resolv,
+                                 'wall_color': 0xFFFFFFFF,
+                                 'paff_color': 0xFF123456,
+                                 'background_color': 0x5F000000})
 
-    def start_loop(self):
+    def start_loop(self) -> None:
         self.__mx.mlx_loop(self.__mlx_ptr)
